b79f4824c7bb204521e36edd4ebb2a948632e60b
ldrl <drdanielreyes5@gmail.com>
2023-09-21 20:01:38 -0600

    Modulo 5

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -4,26 +4,32 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-#Exception
+# Exception
 from django.db.utils import IntegrityError
 
 # Create your views here.
 from django.contrib.auth.models import User
 from users.models import Profile
 
+#Forms
+from users.forms import ProfileForm, SignupForm
+
+
+
 def login_view(request):
     """Login view."""
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username =username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
             return redirect('feed')
         else:
-            return render(request, 'users/login.html',{'error': 'Invalid username and password'})
+            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
     return render(request, 'users/login.html')
+
 
 @login_required
 def logout_view(request):
@@ -31,32 +37,48 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+
 def signup(request):
     """Sign up view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirmation = request.POST['password_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    
+    return render(request=request, template_name='users/signup.html',
+                  context={
+                      'form': form,
+                  })
 
-        if password != password_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-        try:
-            user = User.objects.create_user(username=username, password=password)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
-        
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-        
-        profile = Profile(user=user)
-        profile.save()
 
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
-
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
+
+    
+
+    return render(request, template_name='users/update_profile.html', 
+                  context={'profile': profile, 
+                           'user': request.user,
+                           'form':form,
+                           }
+    )
